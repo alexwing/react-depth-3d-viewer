5c475d6b590daec1616ea23cc107e821b8cf9147
Alejandro Aranda <alexwing@gmail.com>
2021-01-14 14:43:21 +0100

    Fix url parameter

--- src/App.js
+++ src/App.js
@@ -7,38 +7,30 @@ import Depth360Viewer from './components/Depth3DViewer';
 
 
 class Main extends Component {
-  constructor() {
-    super();
-    this.state = {
-      puzzleSelected: 0,
-      image: "./images/test01.jpg",
-      depthImage: "./images/test01_depth.jpg",
-      width: window.innerWidth,
-      height: window.innerHeight
-    };
-  }
-  componentDidMount() {
-    window.addEventListener("resize", this.resizeCanvas.bind(this));
+  constructor(props) {
+    super(props);
 
-    //get url param
+    let selected = 0;
+    //if get url param
     if (window.location.pathname) {
-      //  console.log(window.location.search.substr(5));
-      let selected = 0;
       this.props.content.images.forEach(function (value, index) {
         if (value.url === window.location.search.substr(5)) {
-          //console.log(value.url + "==" + window.location.pathname.substring(1));
           selected = index;
         }
       });
-      this.loadDepthImage(selected);
     }
+    this.state = {
+      puzzleSelected: selected,
+      image: this.props.content.images[selected].image,
+      depthImage: this.props.content.images[selected].depthImage,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+
   }
-  loadDepthImage(index) {
-    this.setState({
-      puzzleSelected: index,
-      image: this.props.content.images[index].image,
-      depthImage: this.props.content.images[index].depthImage,
-    });
+
+  componentDidMount() {
+    window.addEventListener("resize", this.resizeCanvas.bind(this));
   }
 
   onSelectImageHandler = (val) => {
